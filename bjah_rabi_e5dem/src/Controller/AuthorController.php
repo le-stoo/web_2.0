db95aa9f58c77646c16b86d4c8e663a8d93f7f73
si_stoo <jouini.b.mustapha@gmail.com>
2025-09-25 18:28:34 +0100

    Track all files under web2.0

---
+++ bjah_rabi_e5dem/src/Controller/AuthorController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+// slemmmm
+
+final class AuthorController extends AbstractController
+{
+    #[Route('/author', name: 'app_author')]
+    public function index(): Response
+    {
+        return $this->render('author/index.html.twig', [
+            'controller_name' => 'AuthorController',
+        ]);
+    }
+
+    #[Route('/show/{name}', name: 'show_author')]
+    public function showAuthor($name): Response
+    {
+        return $this->render('author/show.html.twig', [
+            'name' => $name
+        ]);
+    }
+}
