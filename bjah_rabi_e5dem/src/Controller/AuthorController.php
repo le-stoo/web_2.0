de03f2ffe600335540ca7af6b50ce4b875ee25ad
si_stoo <jouini.b.mustapha@gmail.com>
2025-09-25 18:31:46 +0100

    2aouil commits7i7a

--- bjah_rabi_e5dem/src/Controller/AuthorController.php
+++ bjah_rabi_e5dem/src/Controller/AuthorController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 // slemmmm
-
+//hehhi
 final class AuthorController extends AbstractController
 {
     #[Route('/author', name: 'app_author')]
